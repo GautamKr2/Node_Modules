37edfe984d4f4a2fbe385f57403862ae4abb409d
GautamKr2 <gautamkumar11008@gmail.com>
2026-08-12 16:32:59 +0530

    Simple Website

--- website.js
+++ website.js
@@ -2,15 +2,22 @@ const http = require('http')
 const fs = require('fs')
 
 http.createServer((req, resp) => {
-    if(req.url == '/') {
-        let data = fs.readFileSync('html/home.html', 'utf-8')
+    let collectHeader = fs.readFileSync('html/header.html', 'utf-8')
+
+    let file = "/home";
+    if(req.url != "/") {
+        file = req.url;
+    }
+    console.log(file)
+    if(req.url != '/style.css') {
+        let data = fs.readFileSync("html"+file+".html", 'utf-8')
         resp.writeHead(200, {'content-type': 'text/html'})
-        resp.write(data)
+        resp.write(collectHeader + data)
     }
     else if(req.url == '/style.css') {
         let data = fs.readFileSync('html/style.css', 'utf-8')
         resp.writeHead(200, {'content-type': 'text/css'})
-        resp.write(data)
+        resp.end(data)
     }
     resp.end()
 }).listen(3200)
